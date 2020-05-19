388824ccb744ca39abe98503214e1c7ed4db62d7
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-05-18 23:54:54 -0600

    Junit clase agregada

---
+++ src/GrafoTest.java
@@ -0,0 +1,17 @@
+/**
+ * @author Bryann Alfaro
+ * Clase donde se realizan las pruebas
+ * unitarias para los metodos del grafo
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GrafoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
